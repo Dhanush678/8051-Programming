ff9527d5e2e7da18c366e9756d8267c742ddefc2
Dhanush gowda u <92075046+Dhanush678@users.noreply.github.com>
2022-07-01 12:51:56 +0530

    Add files via upload

---
+++ c.c
@@ -0,0 +1,18 @@
+#include<reg51.h>
+sbit MYBIT =P2^5;
+void main(){
+while(1){
+unsigned char i;
+unsigned char mess[]="SJCE";
+TMOD=0x20;
+TH1=-3;
+SCON=0x50;
+TR1=1;
+
+for(i=0;i<=4;i=i+1){
+SBUF=mess[i];
+while(TI==0){}
+TI=0;
+}}
+}
+
